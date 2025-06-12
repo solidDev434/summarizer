2bcce99ac157d71637e532e7308a768ee694beb2
Joseph Ibok <josephibok75@gmail.com>
2025-06-12 20:36:08 +0100

    added favicon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body
         className={`${openSans.variable} antialiased`}
       >
